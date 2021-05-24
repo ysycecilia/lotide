e27d1d32c5213615c4c702d983ca14b5e3df4505
Cecilia <ysycecilia@gmail.com>
2021-05-24 19:20:52 +0000

    finish with emoji notice

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`🙆🙆‍♂️🙆 Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🙅🙅‍♂️🙅 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
