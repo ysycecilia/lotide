c26d1f96ad5f85cda6e5ea7057c7008b6532d1f5
Cecilia <ysycecilia@gmail.com>
2021-05-26 18:02:54 +0000

    added countOnly() to return the occurance of a property which matchs a given condition

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`🙆🙆‍♂️🙆 Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🙅🙅‍♂️🙅 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    // inside the loop,
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    // only increment count in results if the item is found in itemsToCount 
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
